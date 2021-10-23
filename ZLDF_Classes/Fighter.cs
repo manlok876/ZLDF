13c9ed0d86ec596dd53abe07e99bc2d30fc6aff2
Denis Maximenko <manlok71@gmail.com>
2021-10-23 13:22:57 +0300

    Add Fighter class fields

--- ZLDF_Classes/Fighter.cs
+++ ZLDF_Classes/Fighter.cs
@@ -8,5 +8,33 @@ namespace ZLDF_Classes
 {
 	class Fighter
 	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string FullName { 
+			get
+			{
+				return String.Format("{0} {1}", LastName, FirstName);
+			} 
+		}
+		public DateTime Birthdate { get; set; }
+
+		public string City { get; set; }
+		public Club Club { get; set; }
+
+		public string Email { get; set; }
+		public string Phone { get; set; }
+
+		public Fighter()
+		{
+			FirstName = "Ivan";
+			LastName = "Ivanov";
+			Birthdate = new DateTime(2000, 1, 1);
+
+			City = "City";
+			Club = null;
+
+			Email = "e@mail.com";
+			Phone = "8 800 555 35 35";
+		}
 	}
 }
