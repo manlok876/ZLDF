e7d7c613824b906acc85158725beefcfce373db0
Denis Maximenko <manlok71@gmail.com>
2021-10-23 15:33:45 +0300

    Add basic duel class
    Signed-off-by: Denis Maximenko <manlok71@gmail.com>

---
+++ ZLDF_Classes/Duel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZLDF.Classes
+{
+	public class Duel : Fight
+	{
+		public Fighter FighterOne
+		{
+			get; private set;
+		}
+		public Fighter FighterTwo
+		{
+			get; private set;
+		}
+
+		public override Fighter[] Fighters
+		{
+			get
+			{
+				return new Fighter[2] { FighterOne, FighterTwo };
+			}
+		}
+
+		Duel()
+		{
+			FighterOne = null;
+			FighterTwo = null;
+		}
+
+		Duel(Fighter FirstFighter, Fighter SecondFighter)
+		{
+			FighterOne = FirstFighter;
+			FighterTwo = SecondFighter;
+		}
+	}
+}
