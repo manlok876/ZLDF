194df3eb08ed36b7f6cd50473765c0b22846b571
Denis Maximenko <manlok71@gmail.com>
2021-11-18 17:31:13 +0300

    Add view/view model for application

---
+++ ZLDF_MainHost/ViewModels/AppViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace ZLDF.MainHost.ViewModels
+{
+	internal class AppViewModel : BindableBase
+	{
+		private TournamentViewModel _selectedTournament;
+
+		public TournamentViewModel SelectedTournament
+		{
+			get { return _selectedTournament; }
+			set
+			{
+				SetProperty(ref _selectedTournament, value);
+			}
+		}
+
+		public AppViewModel()
+		{
+			_selectedTournament = new TournamentViewModel();
+		}
+
+	}
+}
