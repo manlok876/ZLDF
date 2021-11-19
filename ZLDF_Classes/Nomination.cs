c547b69cc9bdcbd71b7f9a07ae1899b6e1eeef7f
Denis Maximenko <manlok71@gmail.com>
2021-11-19 21:57:03 +0300

    Add some Nomination properties and ViewModel

--- ZLDF_Classes/Nomination.cs
+++ ZLDF_Classes/Nomination.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Text;
 using Prism.Mvvm;
 
 namespace ZLDF.Classes
 {
-	public class Nomination : BindableBase
+	public abstract class Nomination : Event
 	{
 		// Participants
+		public abstract ImmutableArray<Fighter> Fighters { get; }
 		// Fight rules
+
 		// Matchmaking
+
+		public abstract ImmutableArray<Fight> Fights { get; }
+
 
 		// Judges?
 	}
---
+++ ZLDF_MainHost/ViewModels/NominationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace ZLDF.MainHost.ViewModels
+{
+	internal class NominationViewModel : BindableBase
+	{
+	}
+}
