b7de5c07b2e43500dfd3505159caa2332c443c25
Denis Maximenko <manlok71@gmail.com>
2021-11-21 16:20:25 +0300

    Derive Fight from Event

--- ZLDF_Classes/Event.cs
+++ ZLDF_Classes/Event.cs
@@ -5,7 +5,35 @@ using Prism.Mvvm;
 
 namespace ZLDF.Classes
 {
+	public enum EventState
+	{
+		Unknown,
+		NotStarted,
+		Scheduled,
+		InProgress,
+		Paused,
+		Finished,
+		Cancelled
+	}
+
 	public abstract class Event : BindableBase
 	{
+		private EventState _state;
+		public EventState State
+		{
+			get
+			{
+				return _state;
+			}
+			set
+			{
+				SetProperty(ref _state, value);
+			}
+		}
+
+		protected Event()
+		{
+			_state = EventState.Unknown;
+		}
 	}
 }
--- ZLDF_Classes/Fight.cs
+++ ZLDF_Classes/Fight.cs
@@ -6,20 +6,9 @@ using Prism.Mvvm;
 
 namespace ZLDF.Classes
 {
-	public enum FightState
-	{
-		NotStarted,
-		Scheduled,
-		InProgress,
-		Paused,
-		Finished,
-		Cancelled
-	}
-
-	public abstract class Fight : BindableBase
+	public abstract class Fight : Event
 	{
 		private string _id;
-		private FightState _state;
 
 		/// <summary>
 		/// Id property is used for persistent tracking of fights, especially between different runs
@@ -35,25 +24,12 @@ namespace ZLDF.Classes
 				SetProperty(ref _id, value);
 			}
 		}
-		public FightState State
-		{
-			get
-			{
-				return _state;
-			}
-			set
-			{
-				SetProperty(ref _state, value);
-			}
-		}
-
 		public abstract ImmutableArray<Fighter> Fighters { get; }
 		public abstract float GetFighterScore(Fighter fighter);
 
 		protected Fight()
 		{
 			_id = "noId";
-			_state = FightState.NotStarted;
 		}
 	}
 }
