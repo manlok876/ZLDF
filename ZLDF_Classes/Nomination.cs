18f87744faa1ebca9aef6e6a6368c37dd4f26b2d
Denis Maximenko <manlok71@gmail.com>
2021-11-22 10:12:52 +0300

    Make Nomination class non-abstract (for testing)

--- ZLDF_Classes/Nomination.cs
+++ ZLDF_Classes/Nomination.cs
@@ -6,7 +6,7 @@ using Prism.Mvvm;
 
 namespace ZLDF.Classes
 {
-	public abstract class Nomination : Event
+	public class Nomination : Event
 	{
 		private string _name;
 		public string Name
@@ -19,16 +19,67 @@ namespace ZLDF.Classes
 		}
 
 		// Participants
-		public abstract ImmutableArray<Fighter> Fighters { get; }
+		private List<Fighter> _fighters = new List<Fighter>();
+
+		public ImmutableArray<Fighter> Fighters
+		{
+			get { return _fighters.ToImmutableArray(); }
+		}
+		// TODO: check uniqueness
+		public void AddFighter(Fighter newFighter)
+		{
+			_fighters.Add(newFighter);
+			RaisePropertyChanged("Fighters");
+		}
+		public void AddFighters(IEnumerable<Fighter> newFighters)
+		{
+			foreach (var fighter in newFighters)
+			{
+				_fighters.Add(fighter);
+			}
+			RaisePropertyChanged("Fighters");
+		}
+
+		public void RemoveFighter(Fighter fighterToRemove)
+		{
+			bool bSuccess = _fighters.Remove(fighterToRemove);
+			if (bSuccess)
+			{
+				RaisePropertyChanged("Fighters");
+			}
+		}
+		public void RemoveFighters(IEnumerable<Fighter> fightersToRemove)
+		{
+			bool bSuccess = false;
+			foreach (var fighter in fightersToRemove)
+			{
+				bSuccess = bSuccess || _fighters.Remove(fighter);
+			}
+			if (bSuccess)
+			{
+				RaisePropertyChanged("Fighters");
+			}
+		}
+
+		public void ClearFighters()
+		{
+			if (_fighters.Count < 1)
+			{
+				return;
+			}
+			_fighters.Clear();
+			RaisePropertyChanged("Fighters");
+		}
+
 		// Fight rules
 
 		// Matchmaking
 
-		public abstract ImmutableArray<Fight> Fights { get; }
+		public ImmutableArray<Fight> Fights { get; }
 
 
 		// Judges?
-		protected Nomination()
+		public Nomination()
 		{
 			_name = "Some Nomination";
 		}
