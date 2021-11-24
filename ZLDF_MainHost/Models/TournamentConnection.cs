e23b8f4f675362096ffd6831d7937e71643c94ad
Denis Maximenko <manlok71@gmail.com>
2021-11-24 20:31:51 +0300

    Add TournamentConnection class for tournament DB abstraction

---
+++ ZLDF_MainHost/Models/TournamentConnection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZLDF.MainHost.Models
+{
+	internal enum TournamentConnectionType
+	{
+		Test,
+		SQLite
+	}
+
+	internal class TournamentConnection
+	{
+		public string Name { get; set; }
+
+		public TournamentConnectionType ConnectionType { get; set; }
+
+		public string URL { get; set; }
+
+		TournamentConnection()
+		{
+			Name = "Some Tournament";
+			ConnectionType = TournamentConnectionType.Test;
+			URL = "";
+		}
+	}
+}
