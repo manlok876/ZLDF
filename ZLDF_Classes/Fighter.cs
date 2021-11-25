2231169b89b75da5bf0dae4f67cb244b7cc97c69
Denis Maximenko <manlok71@gmail.com>
2021-11-25 15:09:45 +0300

    Refactor Fight properties

--- ZLDF_Classes/Fighter.cs
+++ ZLDF_Classes/Fighter.cs
@@ -7,29 +7,88 @@ namespace ZLDF.Classes
 {
 	public class Fighter : BindableBase
 	{
-		public string Id { get; set; }
-		public string FirstName { get; set; }
-		public string LastName { get; set; }
-		public string FullName { 
+		private string _firstName;
+		private string _lastName;
+		private DateOnly _birthday;
+		private string _city;
+		private Club _club;
+		private string _email;
+		private string _phone;
+
+		public string Id { get; private set; }
+		public string FirstName
+		{
+			get { return _firstName; }
+			set
+			{
+				SetProperty(ref _firstName, value);
+			}
+		}
+		public string LastName
+		{
+			get { return _lastName; }
+			set
+			{
+				SetProperty(ref _lastName, value);
+			}
+		}
+		public string FullName
+		{
 			get
 			{
 				return $"{LastName} {FirstName}";
 			} 
 		}
-		public DateTime Birthday { get; set; }
+		public DateOnly Birthday
+		{
+			get { return _birthday; }
+			set
+			{
+				SetProperty(ref _birthday, value);
+			}
+		}
 
-		public string City { get; set; }
-		public Club? Club { get; set; }
+		public string City
+		{
+			get { return _city; }
+			set
+			{
+				SetProperty(ref _city, value);
+			}
+		}
+		public Club? Club
+		{
+			get { return _club; }
+			set
+			{
+				SetProperty(ref _club, value);
+			}
+		}
 
-		public string Email { get; set; }
-		public string Phone { get; set; }
+		public string Email
+		{
+			get { return _email; }
+			set
+			{
+				SetProperty(ref _email, value);
+			}
+		}
+
+		public string Phone
+		{
+			get { return _phone; }
+			set
+			{
+				SetProperty(ref _phone, value);
+			}
+		}
 
 		public Fighter()
 		{
-			Id = "noID";
+			Id = Guid.NewGuid().ToString();
 			FirstName = "Ivan";
 			LastName = "Ivanov";
-			Birthday = new DateTime(2000, 1, 1);
+			Birthday = new DateOnly(2000, 1, 1);
 
 			City = "City";
 			Club = null;
