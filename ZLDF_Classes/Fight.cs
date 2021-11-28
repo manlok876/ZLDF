048cd41cf90cece503d8346fe7bacbbc6f673a45
Denis Maximenko <manlok71@gmail.com>
2021-11-28 19:21:50 +0300

    Override Fight.State

--- ZLDF_Classes/Fight.cs
+++ ZLDF_Classes/Fight.cs
@@ -8,6 +8,16 @@ namespace ZLDF.Classes
 {
 	public abstract class Fight : Event
 	{
+		private EventState _fightState = EventState.Unknown;
+
+		public override EventState State
+		{
+			get { return _fightState; }
+			set
+			{
+				SetProperty(ref _fightState, value);
+			}
+		}
 		public abstract ImmutableArray<Fighter> Fighters { get; }
 		public abstract float GetFighterScore(Fighter fighter);
 
