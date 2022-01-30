f24bb7e7efa49b1a9d4f41edb0d5f2edd7e5a558
Denis Maximenko <manlok71@gmail.com>
2022-01-30 13:32:05 +0300

    Add testing board for main features testing

---
+++ ZLDF_MainHost/Testing/ViewModels/MainTestingViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Prism.Mvvm;
+using Prism.Commands;
+using ZLDF.MainHost.Testing.Views;
+
+namespace ZLDF.MainHost.Testing.ViewModels
+{
+	internal class MainTestingViewModel : BindableBase
+	{
+		public GroupsTestViewModel GroupsTestVM
+		{
+			get;
+			private set;
+		}
+
+		public MainTestingViewModel()
+		{
+			GroupsTestVM = new GroupsTestViewModel();
+		}
+	}
+}
