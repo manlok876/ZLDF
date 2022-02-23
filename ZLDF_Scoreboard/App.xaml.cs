b0d66207250b05d9d10f6f4fcc85d39f1f569c97
Denis Maximenko <manlok71@gmail.com>
2022-02-23 20:55:27 +0300

    Add ZLDF_Scoreboard project

---
+++ ZLDF_Scoreboard/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using ZLDF.Scoreboard.ViewModels;
+using ZLDF.Scoreboard.Views;
+
+namespace ZLDF.Scoreboard
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			AppView app = new AppView();
+			AppViewModel context = new AppViewModel();
+			app.DataContext = context;
+			app.Show();
+		}
+	}
+}
