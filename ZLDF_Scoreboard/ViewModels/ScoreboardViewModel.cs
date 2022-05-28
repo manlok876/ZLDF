f272de74a2fd30791d586afa236bfca00725aeb3
Denis Maximenko <manlok71@gmail.com>
2022-05-28 08:22:01 +0300

    Update scoreboard view

--- ZLDF_Scoreboard/ViewModels/ScoreboardViewModel.cs
+++ ZLDF_Scoreboard/ViewModels/ScoreboardViewModel.cs
@@ -8,15 +8,76 @@ using System.Windows.Input;
 using System.Windows.Media;
 using Prism.Mvvm;
 using Prism.Commands;
+using ZLDF.Classes;
 
 namespace ZLDF.Scoreboard.ViewModels
 {
 	internal class ScoreboardViewModel : BindableBase
 	{
+		private readonly FightOperatorViewModel _operatorViewModel;
 
-		public ScoreboardViewModel()
+		public bool AreFightersSwapped { get; set; }
+
+		public int ArenaNumber { get; }
+
+		public Duel CurrentFight
 		{
+			get
+			{
+				Duel? currentDuel = _operatorViewModel.CurrentFight as Duel;
+				return currentDuel ?? new Duel();
+			}
+		}
 
+		public Fighter LeftFighter
+		{
+			get
+			{
+				if (AreFightersSwapped)
+				{
+					return CurrentFight.SecondFighter;
+				}
+				else
+				{
+					return CurrentFight.FirstFighter;
+				}
+			}
+		}
+		
+		public Fighter RightFighter
+		{
+			get
+			{
+				if (AreFightersSwapped)
+				{
+					return CurrentFight.FirstFighter;
+				}
+				else
+				{
+					return CurrentFight.SecondFighter;
+				}
+			}
+		}
+
+		public int LeftScore
+		{
+			get
+			{
+				return (int)CurrentFight.GetFighterScore(LeftFighter);
+			}
+		}
+
+		public int RightScore
+		{
+			get
+			{
+				return (int)CurrentFight.GetFighterScore(RightFighter);
+			}
+		}
+
+		public ScoreboardViewModel(FightOperatorViewModel operatorViewModel)
+		{
+			_operatorViewModel = operatorViewModel;
 		}
 	}
 }
