a239354fb626c2d28a125b963591574bb1ae56b2
Denis Maximenko <manlok71@gmail.com>
2022-06-07 20:54:42 +0300

    Add Color -> SolidColorBrush converter

---
+++ ZLDF_Scoreboard/Converters/ColorToBrushConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ZLDF.Scoreboard.Converters
+{
+	internal class ColorToBrushConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value == null)
+			{
+				return null;
+			}
+
+			if (value is Color)
+			{
+				Color color = (Color)value;
+				return new SolidColorBrush(color);
+			}
+
+			return null;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			SolidColorBrush? brush = (SolidColorBrush) value;
+			if (brush == null)
+			{
+				return Colors.Transparent;
+			}
+			return brush.Color;
+		}
+	}
+}
