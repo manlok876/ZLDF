236a3c6859d6eebf7da6422d858a343c7c916c85
Denis Maximenko <manlok71@gmail.com>
2022-07-02 13:01:53 +0300

    Add new base class for events

---
+++ ZLDF_Classes/BaseEvent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace ZLDF.Classes
+{
+	public abstract class BaseEvent : BindableBase, IEvent
+	{
+		private Guid _id;
+
+		/// <summary>
+		/// Id property is used for persistent tracking of events, especially between different runs
+		/// </summary>
+		public Guid Id
+		{
+			get { return _id; }
+			private set
+			{
+				SetProperty(ref _id, value);
+			}
+		}
+
+		public virtual EventState State
+		{
+			get { return EventState.Unknown; }
+		}
+
+		protected BaseEvent()
+		{
+
+		}
+	}
+}
