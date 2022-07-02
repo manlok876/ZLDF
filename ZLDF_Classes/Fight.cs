d8a6cd1723ac5d630b276cbc339dc8d846d2e595
Denis Maximenko <manlok71@gmail.com>
2022-07-02 13:08:27 +0300

    Inherit Fight from new BaseEvent

--- ZLDF_Classes/Fight.cs
+++ ZLDF_Classes/Fight.cs
@@ -6,17 +6,13 @@ using Prism.Mvvm;
 
 namespace ZLDF.Classes
 {
-	public abstract class Fight : Event
+	public abstract class Fight : BaseEvent
 	{
 		private EventState _fightState = EventState.Unknown;
 
 		public override EventState State
 		{
 			get { return _fightState; }
-			set
-			{
-				SetProperty(ref _fightState, value);
-			}
 		}
 		public abstract IEnumerable<Fighter> Fighters { get; }
 		public abstract float GetFighterScore(Fighter? fighter);
