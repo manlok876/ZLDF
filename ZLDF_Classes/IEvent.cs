342d0d367fd51c0225c1eaf0c07ef346acc25049
Denis Maximenko <manlok71@gmail.com>
2022-07-02 13:01:10 +0300

    Add IEvent interface

---
+++ ZLDF_Classes/IEvent.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZLDF.Classes
+{
+	public enum EventState
+	{
+		Unknown,
+		NotStarted,
+		Scheduled,
+		Cancelled,
+		InProgress,
+		Paused,
+		Aborted,
+		Finished
+	}
+
+	internal interface IEvent
+	{
+		public abstract EventState State
+		{
+			get;
+		}
+
+		public bool HasStarted
+		{
+			get
+			{
+				return State == EventState.InProgress ||
+					State == EventState.Paused ||
+					State == EventState.Aborted ||
+					State == EventState.Finished;
+			}
+		}
+
+		public bool WaitingToStart
+		{
+			get
+			{
+				return State == EventState.NotStarted ||
+					State == EventState.Scheduled ||
+					State == EventState.Paused;
+			}
+		}
+
+		public bool IsOver
+		{
+			get
+			{
+				return State == EventState.Cancelled ||
+					State == EventState.Aborted ||
+					State == EventState.Finished;
+			}
+		}
+
+		public bool IsOverSuccessfully
+		{
+			get { return State == EventState.Finished; }
+		}
+	}
+}
