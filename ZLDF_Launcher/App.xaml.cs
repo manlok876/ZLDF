fe2d108dbc270871f4d46e645fbece35c7e4a011
Denis Maximenko <manlok71@gmail.com>
2022-08-19 21:04:50 +0300

    Add Launcher project (does not work ATM)

---
+++ ZLDF_Launcher/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using ZLDF.Launcher.ViewModels;
+using ZLDF.Launcher.Views;
+
+namespace ZLDF.Launcher
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			LauncherView app = new();
+			LauncherViewModel context = new();
+			app.DataContext = context;
+			app.Show();
+		}
+	}
+}
---
+++ ZLDF_Launcher/ViewModels/LauncherViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Prism.Commands;
+using ZLDF.MainHost;
+using ZLDF.Scoreboard;
+
+namespace ZLDF.Launcher.ViewModels
+{
+	internal class LauncherViewModel
+	{
+		#region Apps
+
+		public ICommand LaunchMainHostCommand { get; private set; }
+		public void LaunchMainHost()
+		{
+			ZLDF.MainHost.App MainHostApp = new();
+			MainHostApp.Run();
+		}
+
+		#endregion // Apps
+
+		public LauncherViewModel()
+		{
+			LaunchMainHostCommand = new DelegateCommand(LaunchMainHost);
+		}
+	}
+}
