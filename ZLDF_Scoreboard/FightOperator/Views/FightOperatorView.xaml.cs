4b3d5b070c126ceb0760f2152a22a60a94882541
Denis Maximenko <manlok71@gmail.com>
2022-08-28 21:18:35 +0300

    FOV: basic key input handling (scores and pause)

--- ZLDF_Scoreboard/FightOperator/Views/FightOperatorView.xaml.cs
+++ ZLDF_Scoreboard/FightOperator/Views/FightOperatorView.xaml.cs
@@ -71,11 +71,23 @@ namespace ZLDF.Scoreboard.FightOperator.Views
 			IsFlipped = !IsFlipped;
 		}
 
+		public Fighter GetLeftFighter()
+		{
+			return IsFlipped ? ViewModel.SecondFighter : ViewModel.FirstFighter;
+		}
+		
+		public Fighter GetRightFighter()
+		{
+			return IsFlipped ? ViewModel.FirstFighter : ViewModel.SecondFighter;
+		}
+
 		public FightOperatorView()
 		{
 			InitializeComponent();
 
 			SwapFightersCommand = new DelegateCommand(SwapFighters);
+			
+			KeyDown += new KeyEventHandler(OnButtonKeyDown);
 		}
 
 		private void FightsList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -87,5 +99,119 @@ namespace ZLDF.Scoreboard.FightOperator.Views
 				ViewModel?.MoveToFight(d);
 			}
 		}
+
+		#region Input
+
+		/*
+			F1,F2,F3 или Q,W,E - прибавить левому бойцу 1, 2, или 3 балла
+			F4 или Z - снять 1 балл
+			F12,F11,F10 или P,O,I - прибавить правому бойцу 1, 2, или 3 балла
+			F9 или M - снять 1 балл
+
+			F5 - отметить обоюдку
+			F6 - убрать обоюдку
+			F8 - перезапуск матча
+			F7 или Y - последний сход
+
+			Пробел - пауза
+
+			F - прибавить 1 секунду
+			G - вычесть 1 секунду
+			J - добавить 1 минуту
+			T - поменять местами цвета
+
+			Все буквы латинские
+		* */
+		private void OnButtonKeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Space)
+			{
+				ViewModel?.StartStop();
+			}
+			else if (e.Key == Key.Q || e.Key == Key.F1)
+			{
+				ViewModel?.IncreaseFighterScore(GetLeftFighter());
+			}
+			else if (e.Key == Key.W || e.Key == Key.F2)
+			{
+				//leftScore += 2;
+				//UpdateScore();
+			}
+			else if (e.Key == Key.E || e.Key == Key.F3)
+			{
+				//leftScore += 3;
+				//UpdateScore();
+			}
+			else if (e.Key == Key.Z || e.Key == Key.F4)
+			{
+				ViewModel?.DecreaseFighterScore(GetLeftFighter());
+			}
+			else if (e.Key == Key.I || e.Key == Key.System)
+			{
+				//rightScore += 3;
+				//UpdateScore();
+			}
+			else if (e.Key == Key.O || e.Key == Key.F11)
+			{
+				//rightScore += 2;
+				//UpdateScore();
+			}
+			else if (e.Key == Key.P || e.Key == Key.F12)
+			{
+				ViewModel?.IncreaseFighterScore(GetRightFighter());
+			}
+			else if (e.Key == Key.M || e.Key == Key.F9)
+			{
+				ViewModel?.DecreaseFighterScore(GetRightFighter());
+			}
+			else if (e.Key == Key.T)
+			{
+				SwapFighters();
+			}
+			else if (e.Key == Key.F)
+			{
+				//timeLeft += new TimeSpan(0, 0, 1);
+				//matchInProgress = true;
+				//UpdateTimer();
+			}
+			else if (e.Key == Key.G)
+			{
+				//timeLeft -= new TimeSpan(0, 0, 1);
+				//matchInProgress = true;
+				//UpdateTimer();
+			}
+			else if (e.Key == Key.J)
+			{
+				//timeLeft += new TimeSpan(0, 1, 0);
+				//matchInProgress = true;
+				//UpdateTimer();
+			}
+			else if (e.Key == Key.F8)
+			{
+				ViewModel?.RestartFight();
+			}
+			else if (e.Key == Key.F5)
+			{
+				//doubleHits++;
+				//UpdateDoubleHits();
+			}
+			else if (e.Key == Key.F6)
+			{
+				//if (doubleHits > 0)
+				//{
+				//	doubleHits--;
+				//	UpdateDoubleHits();
+				//}
+			}
+			else if (e.Key == Key.Y || e.Key == Key.F7)
+			{
+				//timeLeft = currentGamemode.FinalRoundTime;
+				//TimerTextBlock.Background = Brushes.LightGreen;
+				//matchInProgress = true;
+				//UpdateTimer();
+			}
+		}
+
+		#endregion // Input
 	}
 }
