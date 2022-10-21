a044281d7e1cd4c851357dd1ce6464907175717c
Denis Maximenko <manlok71@gmail.com>
2022-10-21 13:49:18 +0300

    Add dialogs for importing/exporting fights to TSV

---
+++ ZLDF_Scoreboard/Views/ImportFightsTSVDialog.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ZLDF.Classes;
+
+namespace ZLDF.Scoreboard.Views
+{
+	/// <summary>
+	/// Interaction logic for ImportFightsTSVDialog.xaml
+	/// </summary>
+	public partial class ImportFightsTSVDialog : Window
+	{
+		public ImportFightsTSVDialog()
+		{
+			InitializeComponent();
+		}
+
+		private List<Duel> _duels = new();
+		public IEnumerable<Duel> Duels
+		{
+			get
+			{
+				return _duels;
+			}
+			private set
+			{
+				_duels = new List<Duel>(value);
+			}
+		}
+
+		private void SubmitButton_Click(object sender, RoutedEventArgs e)
+		{
+			Duels = ZLDFUtils.ParseDuelsFromTSVString(DuelsTextBox.Text);
+
+			DialogResult = true;
+		}
+	}
+}
