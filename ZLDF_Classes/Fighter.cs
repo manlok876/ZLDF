3e4b929c9d22fc85b9c2f22e7747bb32a596da20
Denis Maximenko <manlok71@gmail.com>
2022-10-21 12:25:32 +0300

    Remove contacts and birthday from Fighter class

--- ZLDF_Classes/Fighter.cs
+++ ZLDF_Classes/Fighter.cs
@@ -9,11 +9,8 @@ namespace ZLDF.Classes
 	{
 		private string _firstName;
 		private string _lastName;
-		private DateOnly _birthday;
 		private string _city;
 		private Club? _club;
-		private string _email;
-		private string _phone;
 
 		public Guid Id { get; private set; }
 		public string FirstName
@@ -41,14 +38,6 @@ namespace ZLDF.Classes
 				return $"{LastName} {FirstName}";
 			} 
 		}
-		public DateOnly Birthday
-		{
-			get { return _birthday; }
-			set
-			{
-				SetProperty(ref _birthday, value);
-			}
-		}
 
 		public string City
 		{
@@ -67,24 +56,6 @@ namespace ZLDF.Classes
 			}
 		}
 
-		public string Email
-		{
-			get { return _email; }
-			set
-			{
-				SetProperty(ref _email, value);
-			}
-		}
-
-		public string Phone
-		{
-			get { return _phone; }
-			set
-			{
-				SetProperty(ref _phone, value);
-			}
-		}
-
 		// These properties are used to support many-to-many relationship in EF
 		// because I am a lazy bastard who cannot be bothered to do it right
 		// and because damn EF Core 6.0 for not supporting unidirected many-to-many relationships
@@ -99,13 +70,9 @@ namespace ZLDF.Classes
 		{
 			_firstName = "Ivan";
 			_lastName = "Ivanov";
-			_birthday = new DateOnly(2000, 1, 1);
 
 			_city = "City";
 			_club = null;
-
-			_email = "e@mail.com";
-			_phone = "8 800 555 35 35";
 		}
 	}
 }
