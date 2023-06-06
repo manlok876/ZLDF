420460cb9e81b031831e26f03473d17c4e73db1d
Denis Maximenko <manlok71@gmail.com>
2023-06-06 17:09:37 +0400

    Add ZLDF.WPF library for common WPF reusables

---
+++ ZLDF_WPF_Library/Converters/FighterToNameConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace ZLDF.WPF.Converters
+{
+	public class FighterToNameConverter : IValueConverter
+	{
+		// TODO: implement proper formatting
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value == null)
+			{
+				return "";
+			}
+
+			if (value is Fighter)
+			{
+				Fighter fighter = (Fighter)value;
+
+				if (parameter is bool)
+				{
+					bool shouldUseFullName = (bool)parameter;
+					if (shouldUseFullName)
+					{
+						return $"{fighter.LastName} {fighter.FirstName}";
+					}
+					else
+					{
+						return $"{fighter.LastName} {fighter.FirstName.Substring(0, 1)}.";
+					}
+				}
+
+				return $"{fighter.LastName} {fighter.FirstName.Substring(0, 1)}.";
+			}
+
+			return "";
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return null;
+		}
+	}
+}
