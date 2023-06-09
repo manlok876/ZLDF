9bb384656de68c529d9b8037360dc078d73cca25
Denis Maximenko <manlok71@gmail.com>
2023-06-09 18:41:31 +0400

    Fix app title

--- ZLDF.App/ViewModels/MainWindowViewModel.cs
+++ ZLDF.App/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ namespace ZLDF.App.ViewModels
 {
 	public class MainWindowViewModel : BindableBase
 	{
-		private string _title = "Prism Application";
+		private string _title = "ZLDF";
 		public string Title
 		{
 			get { return _title; }
