483e9bb9a6fae595c1806b34f2664dee8f31eb4d
Denis Maximenko <manlok71@gmail.com>
2023-06-11 16:38:36 +0400

    Add Club class

---
+++ ZLDF.Core/Club.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism.Mvvm;
+
+namespace ZLDF.Core
+{
+
+	public class Club : BindableBase
+	{
+		public string Name { get; set; }
+
+		public Club()
+		{
+			Name = "-???-";
+		}
+	}
+}
