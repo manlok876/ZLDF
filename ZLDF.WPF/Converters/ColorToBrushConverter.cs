725fc5cb13f8bb80db8799a651287d8f8a2d825f
Denis Maximenko <manlok71@gmail.com>
2023-06-11 16:38:54 +0400

    Add converters from old project

---
+++ ZLDF.WPF/Converters/ColorToBrushConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ZLDF.WPF.Converters
+{
+	public class ColorToBrushConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value == null)
+			{
+				return new SolidColorBrush(Colors.Transparent);
+			}
+
+			if (value is Color)
+			{
+				Color color = (Color)value;
+				return new SolidColorBrush(color);
+			}
+
+			return new SolidColorBrush(Colors.Transparent);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			SolidColorBrush? brush = (SolidColorBrush)value;
+			if (brush == null)
+			{
+				return Colors.Transparent;
+			}
+			return brush.Color;
+		}
+	}
+}
---
+++ ZLDF.WPF/Converters/PersonToNameConverter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Data;
+using ZLDF.Core;
+
+namespace ZLDF.WPF.Converters
+{
+	internal class PersonToNameConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value == null)
+			{
+				return "";
+			}
+
+			// TODO: implement proper formatting
+			bool shouldUseFullName = false;
+			if (parameter is bool)
+			{
+				shouldUseFullName = (bool) parameter;
+			}
+
+			if (value is Person person)
+			{
+				return GetNameStringForPerson(person, shouldUseFullName);
+			}
+			else if (value is IRole role)
+			{
+				if (role.Person != null)
+				{
+					return GetNameStringForPerson(role.Person, shouldUseFullName);
+				}
+				else
+				{
+					return "<Unassigned>";
+				}
+			}
+
+			return "";
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return null;
+		}
+
+		public string GetNameStringForPerson(Person person, bool shouldUseFullName)
+		{
+			if (shouldUseFullName)
+			{
+				return $"{person.LastName} {person.FirstName}";
+			}
+			else
+			{
+				return $"{person.LastName} {person.FirstName.Substring(0, 1)}.";
+			}
+		}
+
+
+	}
+}
