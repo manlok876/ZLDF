7d3edc1b8e3b5d6817f2c160f50b64bbfacd4bb5
Denis Maximenko <manlok71@gmail.com>
2023-08-05 17:22:24 +0400

    Add TournamentView to actually control event after loading

---
+++ ZLDF.MainHost/ViewModels/TournamentViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ZLDF.Core;
+using ZLDF.DataAccess;
+
+namespace ZLDF.MainHost.ViewModels
+{
+	public class TournamentViewModel : BindableBase
+	{
+		private readonly ITournamentDatabase _tournamentDb;
+
+		public TournamentViewModel(ITournamentDatabase tournamentDb)
+		{
+			_tournamentDb = tournamentDb;
+		}
+
+		public Tournament Tournament => _tournamentDb.TournamentObject;
+	}
+}
---
+++ ZLDF.MainHost/Views/TournamentView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ZLDF.MainHost.Views
+{
+	/// <summary>
+	/// Interaction logic for PrismUserControl1
+	/// </summary>
+	public partial class TournamentView : UserControl
+	{
+		public TournamentView()
+		{
+			InitializeComponent();
+		}
+	}
+}
