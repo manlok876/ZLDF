d4003f358f17b29688f6097dfa9574deb734d9dc
Denis Maximenko <manlok71@gmail.com>
2023-08-05 16:28:28 +0400

    Add Tournament and Nomination classes

---
+++ ZLDF.Core/Nomination.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace ZLDF.Core
+{
+	public class Nomination : BindableBase
+	{
+		private Guid _id = Guid.NewGuid();
+		public Guid Id
+		{
+			get => _id;
+			set { SetProperty(ref _id, value); }
+		}
+
+		private string _title = "New Nomination";
+		public string Title
+		{
+			get => _title;
+			set { SetProperty(ref _title, value); }
+		}
+
+		private IList<Person> _participants = new List<Person>();
+		public IEnumerable<Person> Participants
+		{
+			get => _participants;
+			set { SetProperty(ref _participants, new List<Person>(value)); }
+		}
+
+	}
+}
---
+++ ZLDF.Core/Tournament.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace ZLDF.Core
+{
+	public class Tournament : BindableBase
+	{
+		private Guid _id = Guid.NewGuid();
+		public Guid Id
+		{
+			get => _id;
+			set { SetProperty(ref _id, value); }
+		}
+
+		private string _title = "New Tournament";
+		public string Title
+		{
+			get => _title;
+			set { SetProperty(ref _title, value); }
+		}
+
+		private IList<Person> _participants = new List<Person>();
+		public IEnumerable<Person> Participants
+		{
+			get => _participants;
+			set { SetProperty(ref _participants, new List<Person>(value)); }
+		}
+
+		private IList<Nomination> _nominations = new List<Nomination>();
+		public IEnumerable<Nomination> Nominations
+		{
+			get => _nominations;
+			set { SetProperty(ref _nominations, new List<Nomination>(value)); }
+		}
+
+	}
+}
