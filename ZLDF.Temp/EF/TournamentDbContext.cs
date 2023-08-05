28554f954408a8c754fb8ab08eed2062b9a50d0a
Denis Maximenko <manlok71@gmail.com>
2023-08-05 16:36:08 +0400

    Add some old classes for EF testing

---
+++ ZLDF.Temp/EF/TournamentDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Media.Media3D;
+
+using Microsoft.EntityFrameworkCore;
+
+using ZLDF.Core;
+
+namespace ZLDF.Temp.EF
+{
+	public class TournamentDbContext : DbContext
+	{
+		public string ConnectionString { get; private set; }
+
+		internal DbSet<Tournament> Tournaments { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlite(ConnectionString);
+			base.OnConfiguring(optionsBuilder);
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+		}
+		public static string GetConnectionString(TournamentConnection tournamentConnection)
+		{
+			string result = $"Data Source={tournamentConnection.URL}";
+			return result;
+		}
+
+		public TournamentDbContext(TournamentConnection tournamentConnection)
+		{
+			ConnectionString = GetConnectionString(tournamentConnection);
+		}
+
+		public TournamentDbContext(string url)
+		{
+			ConnectionString = $"Data Source={url}";
+		}
+	}
+}
