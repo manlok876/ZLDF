9155621c4423ebf639f5aa4a74251435a8c7301a
Denis Maximenko <manlok71@gmail.com>
2023-08-06 11:58:45 +0400

    Reparent TournamentDbContext onto BaseDbContext

--- ZLDF.Temp/EF/TournamentDbContext.cs
+++ ZLDF.Temp/EF/TournamentDbContext.cs
@@ -1,27 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web;
-using System.Windows.Media.Media3D;
 
 using Microsoft.EntityFrameworkCore;
 
 using ZLDF.Core;
 using ZLDF.DataAccess;
+using ZLDF.DataAccess.EF;
 
 namespace ZLDF.Temp.EF
 {
-	public class TournamentDbContext : DbContext
+	public class TournamentDbContext : BaseDbContext
 	{
-		public string ConnectionString { get; private set; }
-
 		public DbSet<Tournament> Tournaments { get; set; }
 
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
-			optionsBuilder.UseSqlite(ConnectionString);
 			base.OnConfiguring(optionsBuilder);
 		}
 
@@ -29,20 +23,12 @@ namespace ZLDF.Temp.EF
 		{
 		}
 
-		public static string GetConnectionString(DatabaseReference dbReference)
+		public TournamentDbContext(DatabaseReference dbReference) : base(dbReference)
 		{
-			string result = $"Data Source={dbReference.URL}";
-			return result;
 		}
 
-		public TournamentDbContext(DatabaseReference dbReference)
+		public TournamentDbContext(string url) : base(url)
 		{
-			ConnectionString = GetConnectionString(dbReference);
-		}
-
-		public TournamentDbContext(string url)
-		{
-			ConnectionString = $"Data Source={url}";
 		}
 	}
 }
