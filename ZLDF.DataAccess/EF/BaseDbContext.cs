e8bdbde17075f19b68072d995bc3c5c704fb902c
Denis Maximenko <manlok71@gmail.com>
2023-08-06 10:33:15 +0400

    Add base EF DB context for ORM operations

---
+++ ZLDF.DataAccess/EF/BaseDbContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using ZLDF.Core;
+
+namespace ZLDF.DataAccess.EF
+{
+	public class BaseDbContext : DbContext
+	{
+		public string ConnectionString { get; private set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlite(ConnectionString);
+			base.OnConfiguring(optionsBuilder);
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+		}
+
+		public static string GetConnectionString(DatabaseReference databaseReference)
+		{
+			string result = $"Data Source={databaseReference.URL}";
+			return result;
+		}
+
+		public BaseDbContext(DatabaseReference databaseReference)
+		{
+			ConnectionString = GetConnectionString(databaseReference);
+		}
+
+		public BaseDbContext(string url)
+		{
+			ConnectionString = $"Data Source={url}";
+		}
+	}
+}
