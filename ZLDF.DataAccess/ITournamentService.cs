dceff411963abc4f845e2f84ff38072831412735
Denis Maximenko <manlok71@gmail.com>
2023-08-06 15:26:55 +0400

    Add ITournamentService interface

---
+++ ZLDF.DataAccess/ITournamentService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ZLDF.Core;
+
+namespace ZLDF.DataAccess
+{
+	public interface ITournamentService
+	{
+		public Tournament Tournament { get; }
+		public IEnumerable<Nomination> Nominations { get; }
+
+		public Tournament CreateNewTournament();
+		public Tournament LoadTournament();
+		public void SetTournamentTitle(string newTitle);
+	}
+}
