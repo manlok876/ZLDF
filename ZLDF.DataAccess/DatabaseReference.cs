17731e2a09c45b372ea489d814820e34aeb7f490
Denis Maximenko <manlok71@gmail.com>
2023-08-06 10:32:03 +0400

    Add a class to keep database references

---
+++ ZLDF.DataAccess/DatabaseReference.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZLDF.DataAccess
+{
+	public enum DatabaseType
+	{
+		SQLite
+	}
+
+	public class DatabaseReference
+	{
+		public DatabaseType ConnectionType { get; set; }
+
+		public string URL { get; set; }
+
+		internal DatabaseReference()
+		{
+			ConnectionType = DatabaseType.SQLite;
+			URL = "";
+		}
+
+		internal DatabaseReference(string url)
+		{
+			URL = url;
+			ConnectionType = DatabaseType.SQLite;
+		}
+	}
+}
