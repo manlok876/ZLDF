5c2171f409c6e9c74c04d592d741b311d9a1de78
Denis Maximenko <manlok71@gmail.com>
2023-09-09 18:28:03 +0400

    Add db context for manipulating people db

---
+++ ZLDF.Temp/EF/PeopleDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.EntityFrameworkCore;
+
+using ZLDF.Core;
+using ZLDF.DataAccess;
+using ZLDF.DataAccess.EF;
+
+namespace ZLDF.Temp.EF
+{
+	internal class PeopleDbContext : BaseDbContext
+	{
+		public PeopleDbContext(DatabaseReference dbReference) : base(dbReference)
+		{
+		}
+
+		public PeopleDbContext(string url) : base(url)
+		{
+		}
+
+		public DbSet<Person> Participants { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
