bfa3283eaf4048ddb82f23ee1f45d315c86b98db
Denis Maximenko <manlok71@gmail.com>
2023-12-10 11:36:47 +0400

    Add all-in-one DB context

---
+++ ZLDF.DataAccess/EF/FullDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ZLDF.Core;
+
+namespace ZLDF.DataAccess.EF
+{
+	public class FullDbContext : BaseDbContext
+	{
+		public FullDbContext(DatabaseReference dbReference) : base(dbReference)
+		{
+		}
+
+		public FullDbContext(string url) : base(url)
+		{
+		}
+
+		public DbSet<Tournament> Tournaments { get; set; }
+		public DbSet<Person> People { get; set; }
+
+	}
+}
